8d7fab8cb358e764cfbc81334d125aefb0524ab0
maxi <max.lengdell@gmail.com>
2020-10-19 17:50:16 +0200

    test

--- Test/Test.java
+++ Test/Test.java
@@ -1,5 +1,11 @@
 package Homeexam.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class Test {
-    
+    @Test
+    public void test123(){
+        assertEqual(1,1);
+    }
 }
